0c70b3ce66c37c66a9a4c71b961679f60d121e6c
chung1210 <38287866+chung1210@users.noreply.github.com>
2018-12-17 16:52:06 +0700

    first commit

---
+++ ShopOnline/Models/BUS/ShopOnlineBUS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ShopOnlineConnection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopOnline.Models.BUS
+{
+    public class ShopOnlineBUS
+    {
+        public static IEnumerable <SanPham> DanhSach()
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.Query<SanPham>("select * from SanPham where TinhTrang = 0");
+        }
+        public static SanPham ChiTiet(string a)
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.SingleOrDefault<SanPham>("select * from SanPham where MaSanPham = @0",a);
+        }
+        public static IEnumerable<SanPham> HotItems()
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.Query<SanPham>("select Top 8 * from SanPham where LuotView>0");
+        }
+    }
+}
